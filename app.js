84f7622d2c6b842ecd493e6c2f2f32d955a68c45
khaledyd <77590422+khaledyd@users.noreply.github.com>
2022-06-24 16:27:52 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+const imyFile = document.querySelector("#myFile");
+
+myFile.addEventListener("change", function() {
+  const reader = new FileReader();
+  reader.addEventListener("load", () => {
+    const uploaded_image = reader.result;
+    document.querySelector("#display-image").style.backgroundImage = `url(${uploaded_image})`;
+  });
+  reader.readAsDataURL(this.files[0]);
+});
+
+
+
+
+
